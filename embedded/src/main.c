00116be641f6ddda9b7ae5c0bd0ef731f1218ce9
Noah Kim <noahbkim@gmail.com>
2019-11-09 13:20:23 -0800

    As I feared; latency issues

--- embedded/src/main.c
+++ embedded/src/main.c
@@ -8,14 +8,32 @@
 
 SERIAL_SETUP(0)
 
+void adc_setup()
+{
+    adc_set_reference_voltage(ADC_REFERENCE_AVCC);  // AVcc
+    adc_set_prescaler(ADC_PRESCALER_128);           // 128 cycles per ADC clock cycle
+    adc_set_alignment(ADC_ALIGNMENT_LEFT);          // We'll only read top 8 bits
+    adc_enable();                                   // Start
+}
+
 int main()
 {
+    adc_setup();
     serial_construct(S0, 9600, SERIAL_8N1);
-    for (int i = 0; i < 8; i++)
+
+    uint8_t sample = 0;
+    uint8_t debounce = 0;
+    while (true)
     {
-        _delay_ms(500);
-        serial_write(S0, 107);
+        sample = adc_read_left(0);
+        if (sample > 20 && debounce == 0) {
+            serial_write(S0, 107);
+            debounce = 10;
+        } else if (debounce > 0) {
+            --debounce;
+        }
     }
+
     serial_destroy(S0);
     return 0;
 }
